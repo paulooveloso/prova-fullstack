834fc82b136c24c31e7865be3653045b79ffc917
paulooveloso <pauloveloso159@gmail.com>
2025-07-05 21:17:44 -0300

    fix: corrigindo baseURL da API no frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,15 @@
-
 import axios from 'axios';
 
 const api = axios.create({
- 
-  baseURL: 'http://localhost:3001' 
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 
 api.interceptors.request.use(async (config) => {
-
   const token = localStorage.getItem('token');
-  if (token) { 
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  
   return config;
 });
 
